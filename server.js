fef254e74748464dc3b66ccef1fb92067c2d0fa4
martyheyn <heynmarty@gmail.com>
2021-05-03 11:57:59 -0400

    backpacking v1.0.3

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const port = process.env.PORT || 5000;
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
   });
 }
 
