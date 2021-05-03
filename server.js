9482e71469c65b76351ac3996965f86fdff7aaae
martyheyn <heynmarty@gmail.com>
2021-05-03 12:13:52 -0400

    backpacking v1.0.4

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const path = require('path');
 // Initialize Middleware for body parser (to get data from api requests)
 app.use(express.json({ extended: false }));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Test on home
 app.get('/', (req, res) => res.send('API Running'));
 
@@ -12,15 +22,5 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/sack', require('./routes/api/sack'));
 
 const port = process.env.PORT || 5000;
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, '/client/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
-  });
-}
 
 app.listen(port, () => console.log(`Server started on PORT ${port}`));
